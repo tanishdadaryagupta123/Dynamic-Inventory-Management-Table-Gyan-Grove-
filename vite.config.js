21535eb4a9f4ad3fc8dba4dba7f63d60e33734bd
Tanish Dadarya Gupta <114875082+tanishdadaryagupta123@users.noreply.github.com>
2025-01-25 15:11:21 +0530

    shashish

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ],
-    },
+    postcss: './postcss.config.js'
   },
   build: {
     outDir: 'dist',
