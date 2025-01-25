d1b24ee605cdb50aab4698b78968e9a5114665a7
Tanish Dadarya Gupta <114875082+tanishdadaryagupta123@users.noreply.github.com>
2025-01-25 13:33:55 +0530

    shashish

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    middlewareMode: true,
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  }
 })
